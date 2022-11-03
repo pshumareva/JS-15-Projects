0b40fd5c994978968fa976a6580e6c006e34e044
pepashumareva <pepa.shumareva@mentormate.com>
2022-11-03 14:19:54 +0200

    Modal challenge

--- Modal/app.js
+++ Modal/app.js
@@ -2,3 +2,19 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click', function(){
+    modalOverlay.style.visibility = "visible";
+    modalOverlay.style.zIndex= "10";
+    modalOverlay.classList.add('open-modal')
+})
+
+closeBtn.addEventListener('click', function() {
+    modalOverlay.style.visibility = "hidden";
+    modalOverlay.style.zIndex= "-10";
+    modalOverlay.classList.remove('open-modal')
+})
