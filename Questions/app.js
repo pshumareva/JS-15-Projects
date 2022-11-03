528f35935cd69a36cd72c89a2f5d5cb22e9fb1e6
pepashumareva <pepa.shumareva@mentormate.com>
2022-11-03 15:34:28 +0200

    Q&A

--- Questions/app.js
+++ Questions/app.js
@@ -1,2 +1,20 @@
-//using selectors inside the element
-// traversing the dom
+//using selectors inside the element - BETTER WAY - using question instead of document so we dont search throughout the whole document
+ const questions = document.querySelectorAll('.question');
+ questions.forEach(function(question){
+    const btn = question.querySelector('.question-btn')
+    btn.addEventListener('click', function(){
+        question.classList.toggle("show-text");
+    })
+ })
+
+
+
+// // traversing the dom
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//       e.currentTarget.parentElement.classList.toggle("show-text");
+//   });
+// });
